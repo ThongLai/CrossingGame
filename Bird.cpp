959a5ee6b8fc0e892e94c77b178d4f93a1e9a9d7
anhtai311098 <87892927+anhtai311098@users.noreply.github.com>
2021-11-10 21:34:15 +0700

    add file bird.cpp và bird.h

---
+++ Bird.cpp
@@ -0,0 +1,150 @@
+﻿#include"Bird.h"
+
+Bird::Bird(int x, int y)
+{
+	setX(x);
+	setY(y);
+}
+
+void Bird::initDraw()
+{
+	int x = X();
+	int y = Y();
+	ConsoleTask::ConsoleTask::gotoXY(x, y);
+	for (int i = x; i <= x + 14; ++i)
+	{
+		for (int j = y; j <= y + 4; ++j)
+		{
+			ConsoleTask::gotoXY(i, j, char(table[j - y][i - x]));
+		}
+	}
+}
+
+void Bird::drawMoveLeft()
+{
+	setX(X() - 1);
+	int x = X();
+	int y = Y();
+	
+	if (x == -1)
+	{
+		x = 124;
+		setX(x);
+	}
+	
+	if (x + 14 >= 124)
+	{
+		//Xoa
+		for (int j = y; j <= y + 4; ++j)
+		{
+			ConsoleTask::gotoXY(x + 15 - 125, j, char(32));
+		}
+
+		//Them
+		int count = 0;
+		for (int i = x; i <= 124; ++i)
+		{
+			for (int j = y; j <= y + 4; ++j)
+			{
+				ConsoleTask::gotoXY(i, j, char(table[j - y][i - x]));
+			}
+			++count;
+		}
+
+		for (int i = 0; i < x + 14 - 124; ++i)
+		{
+			for (int j = y; j <= y + 4; ++j)
+			{
+				ConsoleTask::gotoXY(i, j, char(table[j - y][count]));
+			}
+			++count;
+			if (count > 14)
+				break;
+		}
+		
+	}
+	else
+	{
+		//Xoa
+		for (int j = y; j <= y + 4; ++j)
+		{
+			ConsoleTask::gotoXY(x + 15, j, char(32));
+		}
+
+		//Them
+		for (int i = x; i <= x + 14; ++i)
+		{
+			for (int j = y; j <= y + 4; ++j)
+			{
+				ConsoleTask::gotoXY(i, j, char(table[j - y][i - x]));
+			}
+		}
+	}
+}
+		
+void Bird::drawMoveRight()
+{
+	setX(X() + 1);
+	int x = X();
+	int y = Y();
+
+	if (x == 125)
+	{
+		x = 0;
+		setX(x);
+	}
+
+	if (x + 14>= 124)
+	{
+		//Xoa
+		for (int j = y; j <= y + 4; ++j)
+		{
+			ConsoleTask::gotoXY(x - 1, j, char(32));
+		}
+
+		//Them
+		int count = 0;
+		for (int i = x; i <= 124; ++i)
+		{
+			for (int j = y; j <= y + 4; ++j)
+			{
+				ConsoleTask::gotoXY(i, j, char(table[j - y][i - x]));
+			}
+			++count;
+		}
+
+		for (int i = 0; i < x + 14 - 124; ++i)
+		{
+			for (int j = y; j <= y + 4; ++j)
+			{
+				ConsoleTask::gotoXY(i, j, char(table[j - y][count]));
+			}
+			++count;
+			if (count > 14)
+				break;
+		}
+
+	}
+	else
+	{
+		//Xoa
+		for (int j = y; j <= y + 4; ++j)
+		{
+			if (x == 0)
+			{
+				ConsoleTask::gotoXY(124, j, char(32));
+			}
+			else ConsoleTask::gotoXY(x - 1, j, char(32));
+			
+		}
+
+		//Them
+		for (int i = x; i <= x + 14; ++i)
+		{
+			for (int j = y; j <= y + 4; ++j)
+			{
+				ConsoleTask::gotoXY(i, j, char(table[j - y][i - x]));
+			}
+		}
+	}
+}
---
+++ Bird.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "ConsoleTask.h"
+
+class Bird
+{
+private:
+	/*const int table[5][15] = { 219, 219, 219, 219, 32, 32, 219, 219, 223, 32, 32, 219, 219, 219, 219,
+							   219, 219, 219, 219, 219, 219, 219, 219, 219, 219, 219, 219, 219, 219, 219,
+							   219, 219, 219, 32, 219, 219, 219, 219, 219, 219, 219, 32, 219, 219, 219,
+							   219, 219, 32, 32, 32, 32, 219, 219, 219, 32, 32, 32, 32, 219, 219,
+							   219, 32, 32, 32, 32, 32, 32, 219, 32, 32, 32, 32, 32, 32, 219 };*/
+
+	string table[5] =
+	{
+		"      /'{>     ",
+		"  ____) (____  ",
+		" //'-;   ;-'\\\\ ",
+		"//////\\_/\\\\\\\\\\\\",
+		"      m m      ",
+	};
+
+	/*string table[5] =
+	{
+		"        _____    ",
+		" ______/__|__\\_  ",
+		"|  ,-.   |  ,-.`|",
+		"|_( o )____( o )|"
+		"   `-'      `-'  "
+	};
+	*/
+
+
+	int x, y;
+public:
+	Bird(int, int);
+
+	int X() { return x; }
+	int Y() { return y; }
+
+	void setX(int x) { this->x = x; }
+	void setY(int y) { this->y = y; }
+
+	void initDraw();
+	void drawMoveLeft();
+	void drawMoveRight();
+	void drawMove() {};
+	//bool isImpact(int, int);
+};
